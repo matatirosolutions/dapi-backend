0c55f261bbc98739c4bd4953cc3315434622060a
Steve Winter <steve@msdev.co.uk>
2019-07-22 10:57:09 +0100

    Restructuring code into more logical order

--- src/Service/FileMakerAPI.php
+++ src/Service/FileMakerAPI.php
@@ -58,38 +58,55 @@ class FileMakerAPI
     }
 
     /**
-     * @param string $layout    The layout to upload content to
-     * @param int $recId        The FileMaker internal recordId of the record to add the content to
-     * @param string $field     The name of the container field
-     * @param string $file      The path to the file to be inserted into the container
-     * @param int $repetition   Which field repetition to use, defaults to 1
-     *
-     * @return array
+     * @param array $params
      *
      * @throws Exception
      */
-    public function performContainerInsert($layout, $recId, $field, $file, $repetition = 1)
+    private function fetchToken(array $params)
     {
-        // Generate the URI required for this request
-        $uri = sprintf('layouts/%s/records/%s/containers/%s/%s', $layout, $recId, $field, $repetition);
-
-        // Se the options specific to uploading a file. This means that we do NOT set the Content-Type
-        // header, but we do still need the Authorization header.
-        // We also have to set a form multipart, which has the name 'upload' as specified in the Data API
-        // documentation, with a 'contents' node containing the actual file data which we read off disk
-        $options = [
-            'headers' => [
-                'Authorization' => sprintf('Bearer %s', $this->retrieveToken()),
-            ],
-            'multipart' => [
-                [
-                    'name' => 'upload',
-                    'contents' => fopen($file, 'r')
+        $client = new Client();
+        try {
+            // Use a Guzzle request to access the FileMaker Data API setting the necessary
+            // headers and credentials
+            $response = $client->request('POST', $this->baseURI . 'sessions', [
+                'headers' => [
+                    'Content-Type' => 'application/json'
                 ],
-            ]
-        ];
+                'auth' => [
+                    $params['username'], $params['password']
+                ]
+            ]);
 
-        return $this->performRequest('POST', $uri, $options);
+            // We get JSON back from the FileMaker Data API. The Guzzle response object stores
+            // that in 'contents' which can be accessed with $response->getBody()->getContents()
+            // we then need to json_decode that so that we can access the object in PHP
+            $content = json_decode($response->getBody()->getContents());
+
+            // The token is in response.token. We need to save that for future use. You could write it
+            // to a file, but because we want this to be user-specific one good place to store it is
+            // in the user's session
+            $this->saveToken($content->response->token);
+        } catch (Exception $exception) {
+            // We need to catch any exceptions which occur and try and provide more useful feedback
+            // The Data API uses standard HTTP responses (see https://httpstatuses.com/)
+
+            /** @var ClientException $exception */
+            if (404 == $exception->getResponse()->getStatusCode()) {
+                // 404 is 'not found' and that's what gets returned when the user credentials are wrong
+                throw new Exception($exception->getResponse()->getReasonPhrase(), 404);
+            }
+
+            // FileMaker is also really good at giving other error information if things go wrong. That's also
+            // in the body contents which we can get from the exception using
+            // $exception->getResponse()->getBody()->getContents()
+            $content = json_decode($exception->getResponse()->getBody()->getContents());
+
+            // Extract the message and throw an exception which we can use in the calling method
+            // to access information about what went wrong.
+            throw new Exception($content->messages[0]->message, $content->messages[0]->code);
+        } catch (GuzzleException $exception) {
+            throw new Exception('Unknown error', -1);
+        }
     }
 
     /**
@@ -152,55 +169,38 @@ class FileMakerAPI
     }
 
     /**
-     * @param array $params
+     * @param string $layout    The layout to upload content to
+     * @param int $recId        The FileMaker internal recordId of the record to add the content to
+     * @param string $field     The name of the container field
+     * @param string $file      The path to the file to be inserted into the container
+     * @param int $repetition   Which field repetition to use, defaults to 1
+     *
+     * @return array
      *
      * @throws Exception
      */
-    private function fetchToken(array $params)
+    public function performContainerInsert($layout, $recId, $field, $file, $repetition = 1)
     {
-        $client = new Client();
-        try {
-            // Use a Guzzle request to access the FileMaker Data API setting the necessary
-            // headers and credentials
-            $response = $client->request('POST', $this->baseURI . 'sessions', [
-                'headers' => [
-                    'Content-Type' => 'application/json'
+        // Generate the URI required for this request
+        $uri = sprintf('layouts/%s/records/%s/containers/%s/%s', $layout, $recId, $field, $repetition);
+
+        // Se the options specific to uploading a file. This means that we do NOT set the Content-Type
+        // header, but we do still need the Authorization header.
+        // We also have to set a form multipart, which has the name 'upload' as specified in the Data API
+        // documentation, with a 'contents' node containing the actual file data which we read off disk
+        $options = [
+            'headers' => [
+                'Authorization' => sprintf('Bearer %s', $this->retrieveToken()),
+            ],
+            'multipart' => [
+                [
+                    'name' => 'upload',
+                    'contents' => fopen($file, 'r')
                 ],
-                'auth' => [
-                    $params['username'], $params['password']
-                ]
-            ]);
+            ]
+        ];
 
-            // We get JSON back from the FileMaker Data API. The Guzzle response object stores
-            // that in 'contents' which can be accessed with $response->getBody()->getContents()
-            // we then need to json_decode that so that we can access the object in PHP
-            $content = json_decode($response->getBody()->getContents());
-
-            // The token is in response.token. We need to save that for future use. You could write it
-            // to a file, but because we want this to be user-specific one good place to store it is
-            // in the user's session
-            $this->saveToken($content->response->token);
-        } catch (Exception $exception) {
-            // We need to catch any exceptions which occur and try and provide more useful feedback
-            // The Data API uses standard HTTP responses (see https://httpstatuses.com/)
-
-            /** @var ClientException $exception */
-            if (404 == $exception->getResponse()->getStatusCode()) {
-                // 404 is 'not found' and that's what gets returned when the user credentials are wrong
-                throw new Exception($exception->getResponse()->getReasonPhrase(), 404);
-            }
-
-            // FileMaker is also really good at giving other error information if things go wrong. That's also
-            // in the body contents which we can get from the exception using
-            // $exception->getResponse()->getBody()->getContents()
-            $content = json_decode($exception->getResponse()->getBody()->getContents());
-
-            // Extract the message and throw an exception which we can use in the calling method
-            // to access information about what went wrong.
-            throw new Exception($content->messages[0]->message, $content->messages[0]->code);
-        } catch (GuzzleException $exception) {
-            throw new Exception('Unknown error', -1);
-        }
+        return $this->performRequest('POST', $uri, $options);
     }
 
     /**
